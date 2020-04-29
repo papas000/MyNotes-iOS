6a8c6de2007b1ba9908123e49dd277237fdbcdcd
papas000 <papas000@gmail.com>
2020-04-29 22:58:13 +0200

    Initial Commit

---
+++ My Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Notes
+//
+//  Created by Au Pair on 4/29/20.
+//  Copyright © 2020 papas000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
